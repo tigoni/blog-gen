2537381c0301b3ce13d2fb06e87615cdc55c35bc
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-07-30 12:49:34 +0300

    export module from library and remove unused functions

--- src/HTMLPrinter.hs
+++ src/HTMLPrinter.hs
@@ -1,23 +1,21 @@
-module HTMLPrinter where
+module HTMLPrinter 
+( Html
+, Tag
+, Title
+, html_
+, p_
+, h1_
+, append_
+, render
+) 
+where
 
-newtype Tag = Tag String
 newtype Html = Html String
-
+newtype Tag = Tag String
 type Title = String
 
 html_ :: Title -> Tag -> Html 
 html_ title content = Html ( element_ "html" (element_ "head" (element_ "title" title) <> element_ "body" (getTagString content)))
-
-head_ :: String -> String
-head_ headContent = "<head>" <> headContent <> "</head>"
-
-title_ :: String -> String
-title_ title = "<title>" <> title <> "</title>"
-
-
---use partial application for function exprr
-body_ :: String -> String 
-body_ = element_ "body"
 
 --add paragraph tag
 p_ :: String -> Tag
@@ -27,12 +25,6 @@ p_ = Tag . element_ "p"
 h1_ :: String -> Tag 
 h1_ = Tag . element_ "h1"
 
-
---utility function to add specified tag to content
-element_ :: String -> String -> String
-element_ tag content = 
-    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
 append_ :: Tag -> Tag -> Tag
 append_ tagStr tagStr' = Tag (getTagString tagStr <> getTagString tagStr')
 
@@ -40,6 +32,13 @@ render :: Html -> String
 render html = 
     case html of 
         Html str -> str
+
+
+--internal utility functions used by this lib
+
+element_ :: String -> String -> String
+element_ tag content = 
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
 getTagString :: Tag -> String
 getTagString string = 
