d6b9049f4cfb89350dee54f9918d5f79969f7405
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-08-07 15:31:52 +0300

    task: add sample text for formatting with ml

--- src/HTMLPrinter.hs
+++ src/HTMLPrinter.hs
@@ -1,69 +1,66 @@
-module HTMLPrinter 
-( Html
-, Tag
-, Title
-, html_
-, p_
-, h1_
-, append_
-, render
-) 
+module HTMLPrinter
+  ( Html,
+    Tag,
+    Title,
+    html_,
+    p_,
+    h1_,
+    append_,
+    render,
+  )
 where
 
 newtype Html = Html String
+
 newtype Tag = Tag String
+
 type Title = String
 
-html_ :: Title -> Tag -> Html 
-html_ title content = Html ( element_ "html" (element_ "head" (element_ "title" $ escape title) <> element_ "body" (getTagString  content)))
+html_ :: Title -> Tag -> Html
+html_ title content = Html (element_ "html" (element_ "head" (element_ "title" $ escape title) <> element_ "body" (getTagString content)))
 
---wrap a string inside a paragraph tag `<p>str</p>`
+-- wrap a string inside a paragraph tag `<p>str</p>`
 p_ :: String -> Tag
 p_ = Tag . element_ "p" . escape
 
---wrap a string inside a header tag `<h1>str</h1>`
-h1_ :: String -> Tag 
+-- wrap a string inside a header tag `<h1>str</h1>`
+h1_ :: String -> Tag
 h1_ = Tag . element_ "h1" . escape
 
 -- ul_ :: [Tag] -> Tag
 -- ul tagList = tagList
 
-
---concatenate the strings of two Tags
+-- concatenate the strings of two Tags
 append_ :: Tag -> Tag -> Tag
 append_ tagStr tagStr' = Tag (getTagString tagStr <> getTagString tagStr')
 
---print/return the contents of a Tag
+-- print/return the contents of a Tag
 render :: Html -> String
-render html = 
-    case html of 
-        Html str -> str
+render html =
+  case html of
+    Html str -> str
 
-
---take a html tag and some content and wrap the content in the tag
+-- take a html tag and some content and wrap the content in the tag
 element_ :: String -> String -> String
-element_ tag content = 
-    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+element_ tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
---Get the string wrapped by a tag
+-- Get the string wrapped by a tag
 getTagString :: Tag -> String
-getTagString string = 
-    case string of 
-        Tag str -> str
+getTagString string =
+  case string of
+    Tag str -> str
 
-
---handle user input that may contain invalid characters that will conflict with HTML tags 
+-- handle user input that may contain invalid characters that will conflict with HTML tags
 
 escape :: String -> String
-escape = 
-    let 
-        escapeChar c = 
-            case c of 
-                '<' -> "&lt;"
-                '>' -> "&gt;"
-                '&' -> "&amp;"
-                '"' -> "&quot"
-                '\''-> "&#39;"
-                _   -> [c]
-        in 
-            concatMap escapeChar
+escape =
+  let escapeChar c =
+        case c of
+          '<' -> "&lt;"
+          '>' -> "&gt;"
+          '&' -> "&amp;"
+          '"' -> "&quot"
+          '\'' -> "&#39;"
+          _ -> [c]
+   in concatMap escapeChar
