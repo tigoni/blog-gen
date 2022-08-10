47e4e0d04d990261392b3178eb099c64b61f2f5c
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-08-10 15:53:05 +0300

    refactor function to handle all markdown cases

--- src/Structure.hs
+++ src/Structure.hs
@@ -14,37 +14,64 @@ data Structure
   | CodeBlock [String]
   deriving (Show)
 
-
-
 --
 parse :: String -> Document
 parse = parseLines Nothing . lines 
 
---group together paragraphs into a structure
-parseLines :: Maybe Structure -> [String] -> Document
-parseLines context text = 
-    case text of
-      [] -> maybeToList context
-      currentLine : rest -> 
-        let line = trim currentLine
-        in
-          if trim line == ""
-          then 
-            maybe id (:) context (parseLines Nothing rest)
-          else
-            case context of 
-              Just (Paragraph paragraph) -> 
-                parseLines (Just (Paragraph (unwords [paragraph, line]))) rest
-              _ -> maybe id (:) context (parseLines (Just (Paragraph line)) rest)
-
-
 trim :: String -> String
 trim = unwords . words
 
+parseLines :: Maybe Structure -> [String] -> Document
+parseLines context txts =
+  case txts of
+    -- done case
+    [] -> maybeToList context
 
+    -- Heading 1 case
+    ('*' : ' ' : line) : rest ->
+      maybe id (:) context (Heading 1 (trim line) : parseLines Nothing rest)
 
+    -- Unordered list case
+    ('-' : ' ' : line) : rest ->
+      case context of
+        Just (UnOrderedList list) ->
+          parseLines (Just (UnOrderedList (list <> [trim line]))) rest
 
+        _ ->
+          maybe id (:) context (parseLines (Just (UnOrderedList [trim line])) rest)
 
+    -- Ordered list case
+    ('#' : ' ' : line) : rest ->
+      case context of
+        Just (OrderedList list) ->
+          parseLines (Just (OrderedList (list <> [trim line]))) rest
+
+        _ ->
+          maybe id (:) context (parseLines (Just (OrderedList [trim line])) rest)
+
+    -- Code block case
+    ('>' : ' ' : line) : rest ->
+      case context of
+        Just (CodeBlock code) ->
+          parseLines (Just (CodeBlock (code <> [line]))) rest
+
+        _ ->
+          maybe id (:) context (parseLines (Just (CodeBlock [line])) rest)
+
+    -- Paragraph case
+    currentLine : rest ->
+      let
+        line = trim currentLine
+      in
+        if line == ""
+          then
+            maybe id (:) context (parseLines Nothing rest)
+          else
+            case context of
+              Just (Paragraph paragraph) ->
+                parseLines (Just (Paragraph (unwords [paragraph, line]))) rest
+              _ ->
+                maybe id (:) context (parseLines (Just (Paragraph line)) rest)
 
 
 
