f9335393ec70fb5e28567b0668950886fe80a049
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-09-09 15:43:01 +0300

    integrate markup adt with parser using convert module

---
+++ src/Convert.hs
@@ -0,0 +1,17 @@
+module Convert where
+    
+    -- Convert Markup data types into HTMLPrinter
+
+import qualified  Markup
+import qualified HTMLPrinter
+
+convertStructure :: Markup.Structure -> HTMLPrinter.Tag
+convertStructure structure = 
+    case structure of
+        Markup.Heading 1 txt -> HTMLPrinter.h1_ txt
+        Markup.Paragraph p -> HTMLPrinter.p_ p
+        Markup.UnOrderedList list -> HTMLPrinter.ul_ $ map HTMLPrinter.p_ list
+        Markup.OrderedList list -> HTMLPrinter.ul_ $ map HTMLPrinter.p_ list
+        Markup.CodeBlock list -> HTMLPrinter.code_ (unlines list)
+
+
