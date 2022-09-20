ece4ab57164f95ce2b727825d98ed87d0e8fe3c0
Kelly Muhindi <kmuhindi@protonmail.ch>
2022-09-20 05:45:50 +0300

    clean up files

--- app/Main.hs
+++ app/Main.hs
@@ -31,21 +31,3 @@ main = do
 
 process :: HTMLPrinter.Title -> String -> String
 process title = HTMLPrinter.render . convert title . Markup.parse
-
-confirm :: IO Bool
-confirm = do
-  putStrLn "Are you sure? (y/n)"
-  answer <- getLine
-  case answer of
-    "y" -> pure True
-    "n" -> pure False
-    _ -> do
-      putStrLn "Invalid response. use y or n"
-      confirm
-
-whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action = do
-  result <- cond
-  if result
-    then action
-    else pure ()
